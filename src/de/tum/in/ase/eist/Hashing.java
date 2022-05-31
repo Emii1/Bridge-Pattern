64a3d170a1fb630168cf59750fdbcd1abc3892e8
Emi <emi.mano@tum.de>
2022-05-31 18:59:00 +0200

    ab

--- src/de/tum/in/ase/eist/Hashing.java
+++ src/de/tum/in/ase/eist/Hashing.java
@@ -1,14 +1,26 @@
 package de.tum.in.ase.eist;
 
-public abstract class Hashing implements HashFunction {
+public abstract class Hashing  {
 
-    @Override
+    private HashFunction implementation;
+
+    public Hashing(HashFunction implementation) {
+        this.implementation = implementation;
+    }
+
+    public HashFunction getImplementation() {
+        return implementation;
+    }
+
+    public void setImplementation(HashFunction implementation) {
+        this.implementation = implementation;
+    }
+
     public String calculateHashCode(String input) {
         return null;
     }
 
 
-    String hashDocument(String input) {
-        return null;
-    }
+
+     public abstract String hashDocument(String input);
 }
