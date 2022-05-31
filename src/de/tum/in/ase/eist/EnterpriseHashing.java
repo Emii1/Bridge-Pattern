4a3134329a7e0e96900e48b8b8ff28b620489281
Emi <emi.mano@tum.de>
2022-05-31 21:24:29 +0200

    ab

---
+++ src/de/tum/in/ase/eist/EnterpriseHashing.java
@@ -0,0 +1,12 @@
+package de.tum.in.ase.eist;
+
+public class EnterpriseHashing extends Hashing{
+    public EnterpriseHashing(HashFunction implementation) {
+        super(implementation);
+    }
+
+    @Override
+    public String hashDocument(String input) {
+        return null;
+    }
+}
---
+++ src/de/tum/in/ase/eist/PreviewHashing.java
@@ -0,0 +1,12 @@
+package de.tum.in.ase.eist;
+
+public class PreviewHashing extends Hashing {
+    public PreviewHashing(HashFunction implementation) {
+        super(implementation);
+    }
+
+    @Override
+    public String hashDocument(String input) {
+        return null;
+    }
+}
